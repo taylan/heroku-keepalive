967578e998b30b43929db1e18bd9476500e6f966
Taylan Aydinli <taylanaydinli@gmail.com>
2014-02-17 11:53:44 +0200

    Added augmented-rss to app list. Added exception handling.

--- keepalive.py
+++ keepalive.py
@@ -1,10 +1,14 @@
 from urllib import request
 from os import environ
 from urllib.parse import urljoin
+from urllib.error import HTTPError
 
-apps = ['chord-search', 'weightmon']
+apps = ['chord-search', 'weightmon', 'augmented-rss']
 app_url_template = 'http://{0}.herokuapp.com'
 for a in apps:
 	print('pinging', a)
-	request.urlopen(app_url_template.format(a)).read()
+	try:
+		request.urlopen(app_url_template.format(a)).read()
+	except HTTPError as err:
+		print('got error', err)
 	print('pinged', a)
