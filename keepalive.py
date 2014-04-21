fa5c0f2bde204204699333cda491faa04f46999a
Taylan Aydinli <taylanaydinli@gmail.com>
2014-04-21 20:52:02 +0300

    Added another project to keepalive script

--- keepalive.py
+++ keepalive.py
@@ -3,7 +3,7 @@ from os import environ
 from urllib.parse import urljoin
 from urllib.error import HTTPError
 
-apps = ['weightmon', 'augmented-rss']
+apps = ['weightmon', 'augmented-rss', 'bombiquecom']
 app_url_template = 'http://{0}.herokuapp.com'
 for a in apps:
 	print('pinging', a)
